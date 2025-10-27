a3150b8fca8c8b66fb111de4f75653b6c5765c9a
fionabf05 <fionabfuller@gmail.com>
2025-10-26 22:00:26 -0600

    homework 9!

---
+++ Homework 9/sketch.js
@@ -0,0 +1,46 @@
+function setup() 
+{
+  createCanvas(400,400);
+  
+}
+
+function draw() 
+{
+  background(220);
+  text('"me if i was just a little sinister" by fiona fuller',10,30);
+  
+  fill(220);
+  strokeWeight(3);
+
+  circle(200,200,250);
+
+  ellipse(200,240,40,20);
+
+  line(160,270,240,270);
+
+  line(160,270,160,260);
+
+  line(240,270,240,260);
+
+  square(80,120,100);
+
+  square(220,120,100);
+
+  line(180,180,220,180);
+
+  triangle(140,170,120,190,160,190);
+  
+  triangle(260,170,240,190,280,190);
+
+  strokeWeight(15);
+
+  point(140,180);
+
+  point(260,180);
+
+  strokeWeight(3);
+
+  textSize(32);
+ 
+
+}
