324ef310ad9dbba9399331c26747e2aa60630672
fionafuller <fionabfuller@gmail.com>
2025-11-13 22:20:08 -0700

    Homework 12!

---
+++ Homework 12/HW12JS.js
@@ -0,0 +1,146 @@
+
+var characterX = 100; 
+var characterY = 100; 
+
+var enemyaX = 200;
+var enemyaY = 200;
+var enemyaXSpeed = 3;
+var enemyaYSpeed = 5;
+
+var enemybX = 300;
+var enemybY = 300;
+var enemybXSpeed = 6;
+var enemybYSpeed = 2;
+
+var mouseShapex;
+var mouseShapey;
+
+function setup()
+{
+    createCanvas(800,600);
+}
+
+function draw() 
+{
+  background(20,100,100);
+
+  createBorders();
+
+  createCharacter();
+  
+  youWin();
+
+  createEnemies();
+
+  moveEnemeies();
+
+  mouseObstacle();
+}
+
+function mouseObstacle()
+{
+  fill(10,50,50);
+  circle(mouseShapex,mouseShapey,25);
+}
+
+function moveEnemeies()
+{
+    if(enemyaX > 800)
+    {
+      enemyaX = 0;
+    }
+    if(enemyaX < 0)
+    { 
+      enemyaX = 800;
+    }
+    if(enemyaY > 600)
+    {
+      enemyaY = 0;
+    }
+    if(enemyaY < 0)
+    { 
+      enemyaY = 600;
+    }
+
+    if(enemybX > 800)
+    {
+      enemybX = 0;
+    }
+    if(enemybX < 0)
+    { 
+      enemybX = 800;
+    }
+    if(enemybY > 600)
+    {
+      enemybY = 0;
+    }
+    if(enemybY < 0)
+    { 
+      enemybY = 600;
+    }
+}
+
+function createEnemies()
+{
+  fill(50,50,10);
+  square(enemyaX,enemyaY,20);
+  enemyaX += enemyaXSpeed;
+  enemyaY += enemyaYSpeed; 
+
+  fill(50,10,50);
+  square(enemybX,enemybY,40);
+  enemybX += enemybXSpeed;
+  enemybY += enemybYSpeed; 
+}
+
+function createBorders()
+{
+  fill(20,100,100);
+  strokeWeight(30);
+  rect(0,0,800,600);
+
+  strokeWeight(0);
+  rect(770,480,30,100);
+}
+
+function youWin()
+{
+   if(characterX > 800 && characterY > 470)
+  {
+    textSize(50);
+    text("YOU WIN!!", 400, 100);
+  }
+}
+
+function createCharacter()
+{
+  fill(255);
+  strokeWeight(3);
+  circle(characterX,characterY,50);
+}
+
+function keyPressed() 
+{
+  if (key == 'd') 
+  {
+    characterX+=50;
+  } 
+  else if (key == 'a') 
+  {
+    characterX-=50;
+  }
+  else if (key == 'w')
+  {
+    characterY-=50;
+  }
+  else if (key == 's')
+  {
+    characterY+=50;
+  }
+}
+
+function mouseClicked()
+{
+    mouseShapex = mouseX;
+    mouseShapey = mouseY;
+}
